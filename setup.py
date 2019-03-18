63b4b871046c72d22f59e80cebf4ef3693c22295
Rick Nitsche <rick@phas.ubc.ca>
2019-03-18 14:34:14 -0700

    stub for ch_util and ch_util.data_index

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import setuptools
+
+setuptools.setup(
+    name='ch_util',
+    author="The CHIME Collaboration",
+    author_email="rick@phas.ubc.ca",
+    description="A stub module to use as a mock import of the private ch_util repository",
+    packages=['ch_util', 'ch_util.data_index'],
+    url="https://github.com/chime-experiment/ch_util-stub"
+)
+

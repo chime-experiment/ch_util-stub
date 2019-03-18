a7050f5846b19954e4e0284cc41428a817242273
Rick Nitsche <rick@phas.ubc.ca>
2019-03-18 14:52:37 -0700

    adds ephemeris and fluxcat stubs

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ setuptools.setup(
     name='ch_util',
     author="The CHIME Collaboration",
     author_email="rick@phas.ubc.ca",
-    description="A stub module to use as a mock import of the private ch_util repository",
-    packages=['ch_util', 'ch_util.data_index'],
+    description="A stub module to use as a mock import of the private ch_util"
+                " repository",
+    packages=['ch_util', 'ch_util.data_index', 'ch_util.ephemeris',
+              'ch_util.fluxcat'],
     url="https://github.com/chime-experiment/ch_util-stub"
 )
 

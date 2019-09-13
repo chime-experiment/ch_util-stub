3ec4b9a0213434625c7fbda5353ef9f0090c5b8f
D. V. Wiebe <dvw@phas.ubc.ca>
2019-09-12 17:19:35 -0700

    ditto

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     author_email="rick@phas.ubc.ca",
     description="A stub module to use as a mock import of the private ch_util"
                 " repository",
-    packages=['ch_util', 'ch_util.data_index', 'ch_util.ephemeris',
+    packages=['ch_util', 'ch_util.finder', 'ch_util.ephemeris',
               'ch_util.fluxcat', 'ch_util.tools'],
     url="https://github.com/chime-experiment/ch_util-stub"
 )

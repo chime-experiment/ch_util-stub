f7584d2879bf48112307565688693f0dddd22da1
D. V. Wiebe <dvw@phas.ubc.ca>
2019-09-12 17:34:02 -0700

    Add andata

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setuptools.setup(
     author_email="rick@phas.ubc.ca",
     description="A stub module to use as a mock import of the private ch_util"
                 " repository",
-    packages=['ch_util', 'ch_util.finder', 'ch_util.ephemeris',
-              'ch_util.fluxcat', 'ch_util.tools'],
+    packages=['ch_util', 'ch_util.andata', 'ch_util.finder',
+        'ch_util.ephemeris', 'ch_util.fluxcat', 'ch_util.tools'],
     url="https://github.com/chime-experiment/ch_util-stub"
 )
 

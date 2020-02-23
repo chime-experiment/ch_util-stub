9374fb371bdcfec23ebeeaf304a4d1c4a1d5ec37
anjakefala <anja.kefala@gmail.com>
2020-02-23 11:54:14 -0800

    feat: add ch_util.cal_utils

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="A stub module to use as a mock import of the private ch_util"
                 " repository",
     packages=['ch_util', 'ch_util.andata', 'ch_util.finder',
-        'ch_util.ephemeris', 'ch_util.fluxcat', 'ch_util.rfi', 'ch_util.tools'],
+        'ch_util.ephemeris', 'ch_util.fluxcat', 'ch_util.rfi', 'ch_util.tools', 'ch_util.cal_utils'],
     url="https://github.com/chime-experiment/ch_util-stub"
 )
 
